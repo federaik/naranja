94dda97c15eb3d01d24b466152794fb6c2752fce
Facundo Ottalagano <facuottalagano@gmail.com>
2021-05-05 16:20:21 -0300

    Refactoring code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,57 @@
+const ENDPOINT_BASE_PATH = 'http://localhost:8081'
+
 let playerIdToEdit = null;
 
 const getPlayers = () => {
-  let fetchPromise = fetch("http://127.0.0.1:8081/players")  
+  let fetchPromise = fetch(`${ENDPOINT_BASE_PATH}/players`)  
   let responsePromise = fetchPromise.then((response) => {
     return response.json()
   })
   responsePromise.then((playersJson) => {
     listPlayers(playersJson)    
   })  
+}
+
+const getPlayerById = (id) => {
+  let fetchPromise = fetch(`${ENDPOINT_BASE_PATH}/players/${id}`)
+  let responsePromise = fetchPromise.then((response) => {
+    return response.json()
+  })
+  responsePromise.then((playerJson) => {
+    showPlayerInfo(playerJson);
+  })  
+}
+
+const putPlayer = () => {
+
+  // Codigo de referencia. Borrar mas adelante
+  // const nombre = document.getElementById('nombre');  
+  // const apellido = document.getElementById('apellido'); 
+  // const posicion = document.getElementById('posicion');
+  const formControls = getFormControls()
+
+  const playerObject = {
+    firstName: formControls.nombre.value, 
+    lastName: formControls.apellido.value,
+    posiciones: formControls.posicion.value.split(',')
+  };    
+  const fetchPromise = fetch(`${ENDPOINT_BASE_PATH}/players/${playerIdToEdit}`, {method: 'PUT', body: JSON.stringify(playerObject), headers: { 'Content-Type': 'application/json' }});
+  const responsePromise = fetchPromise.then((response) => {
+    return response.json();
+  })
+  responsePromise.then((playerJson) => {
+    getPlayers();    
+  })           
+}
+
+const deletePlayer = (id) => {
+  const fetchPromise = fetch(`${ENDPOINT_BASE_PATH}/players/${id}`, {method: 'DELETE'})
+  const responsePromise = fetchPromise.then((response) => {
+    return response.json()
+  })
+  responsePromise.then((playerJson) => {
+    getPlayers()        
+  }) 
 }
 
 const postPlayer = () => {
@@ -21,13 +65,12 @@ const postPlayer = () => {
   // obtener los datos del formulario
   // acordate de convertir el String de posiciones a un Array. (googlear)
   let posiciones = inputPosicion.value.split(',');
- ;
+ 
   // armar un objeto nuevo con la info del jugador para enviarle al BE
-  let newObject = {firstName: inputName.value, lastName: inputLastName.value, posiciones: posiciones}; 
-  console.log(newObject);  
+  let newObject = {firstName: inputName.value, lastName: inputLastName.value, posiciones: posiciones};    
   
   // llamar a fetch 
-  let fetchPromise = fetch('http://localhost:8081/players', {method: 'POST', body: JSON.stringify(newObject), headers: { 'Content-Type': 'application/json' }})
+  let fetchPromise = fetch(`${ENDPOINT_BASE_PATH}/players/`, {method: 'POST', body: JSON.stringify(newObject), headers: { 'Content-Type': 'application/json' }})
   let responsePromise = fetchPromise.then((response) => {
     return response.json()    
   })
@@ -47,79 +90,63 @@ const listPlayers = (players) => {
   document.getElementById('playersList').innerHTML = "";
 
   players.forEach((player) => {
-    let newDiv = document.createElement("div");    
-    let newSpan = document.createElement("span");
-    newSpan.textContent = player.firstName + " " + player.lastName + " " + player.posiciones;    
+    const newDiv = document.createElement("div");    
+    const newSpan = document.createElement("span");   
+    newSpan.textContent = `${player.firstName} ${player.lastName} ${player.posiciones}`
     newSpan.setAttribute('style', 'color: rgb(153, 153, 144)');
     
-    let newButton = document.createElement("button");
-    newButton.textContent = 'View more';
-    newSpan.appendChild(newButton);
-    newButton.onclick = () => {
-      let fetchPromise = fetch("http://localhost:8081/players/" + player.id)
-      let responsePromise = fetchPromise.then((response) => {
-        return response.json()
-      })
-      responsePromise.then((playerJson) => {
-        showPlayerInfo(playerJson);
-      })  
-    }    
-    let currentDiv = document.getElementById("playersList");
+    const viewMoreButton = document.createElement("button");
+    viewMoreButton.textContent = 'View more';
+    newSpan.appendChild(viewMoreButton);
+    viewMoreButton.onclick = () => {
+      getPlayerById(player.id)
+    }
+    const currentDiv = document.getElementById("playersList");
     currentDiv.appendChild(newDiv);    
     newDiv.appendChild(newSpan); 
 
-    let deleteButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
     deleteButton.textContent = 'Delete Player';
     newSpan.appendChild(deleteButton);
     deleteButton.onclick = () => {
-      let fetchPromise = fetch("http://localhost:8081/players/" + player.id, {method: 'DELETE'})
-      let responsePromise = fetchPromise.then((response) => {
-        return response.json()
-      })
-      responsePromise.then((playerJson) => {
-        getPlayers()        
-      })  
+      deletePlayer(player.id)
     }
 
-    let editPlayer = document.createElement('button');
-    editPlayer.textContent = 'edit player';
-    newSpan.appendChild(editPlayer);
+    const editPlayerButton = document.createElement('button');
+    editPlayerButton.textContent = 'edit player';
+    newSpan.appendChild(editPlayerButton);
 
-    editPlayer.onclick = () => {      
-    
-      const nombre = document.getElementById('nombre');  
-      const apellido = document.getElementById('apellido'); 
-      const posicion = document.getElementById('posicion');     
-
-      nombre.value = player.firstName;
-      apellido.value = player.lastName;
-      posicion.value = player.posiciones;    
-
-      playerIdToEdit = player.id;            
+    editPlayerButton.onclick = () => {      
+      populateFormWithPlayer(player)            
     }    
   })  
 }
 
-const putPlayer = () => {
+const populateFormWithPlayer = (player) => {
+  const formControls = getFormControls();
 
+  formControls.nombre.value = player.firstName;
+  formControls.apellido.value = player.lastName;
+  formControls.posicion.value = player.posiciones;    
+
+  playerIdToEdit = player.id;
+}
+
+const getFormControls = () => {
   const nombre = document.getElementById('nombre');  
   const apellido = document.getElementById('apellido'); 
   const posicion = document.getElementById('posicion');
 
-  const playerObject = {firstName: nombre.value, lastName: apellido.value, posiciones: posicion.value.split(',')};
-  console.log(playerObject)
-  let fetchPromise = fetch('http://localhost:8081/players/' + playerIdToEdit, {method: 'PUT', body: JSON.stringify(playerObject), headers: { 'Content-Type': 'application/json' }});
-  let responsePromise = fetchPromise.then((response) => {
-    return response.json();
-  })
-  responsePromise.then((playerJson) => {
-    getPlayers();    
-  })           
+  return {
+    nombre: nombre,
+    apellido: apellido,
+    posicion: posicion
+  };
 }
 
 const showPlayerInfo = (player) => {
-  let divDataPlayer = document.getElementById('playerDetail')
-  divDataPlayer.textContent = player.id + " " + player.firstName + " " + player.lastName + " " + player.posiciones;    
+  const divDataPlayer = document.getElementById('playerDetail')  
+  divDataPlayer.textContent = `${player.id} ${player.firstName} ${player.lastName} ${player.posiciones}`    
 }
 
 const clearForm = () => {
